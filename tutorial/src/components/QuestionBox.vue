22a2a59040319f3d465e05f33d6d7e6ce211e3d6
Phone Nyo <phonenai2014@gmail.com>
2019-07-24 19:10:21 +0630

    fetch api data & passing data to component 1:04:00

--- tutorial/src/components/QuestionBox.vue
+++ tutorial/src/components/QuestionBox.vue
@@ -1,19 +1,22 @@
 <template>
 <div class="question-box-container">
 	<b-jumbotron >
-
 	    <template slot="lead">
-	      Some question here ?
+	    	{{currentQuestion.question}}
 	    </template>
-
 	    <hr class="my-4">
-
 	    <p>
 	      List of answers
 	    </p>
-
 	    <b-button variant="primary" href="#">Submit</b-button>
 	    <b-button variant="success" href="#">Next</b-button>  
 	</b-jumbotron>
 </div>
 </template>
+<script>
+	export default {
+		props: {
+			currentQuestion: Object
+		}
+	}
+</script>
