64f8d89b0fa12cb8b5c5336148c9732bf35dcfdd
Phone Nyo <phonenai2014@gmail.com>
2019-07-24 16:09:25 +0630

    Usage of BootstrapVue

--- tutorial/src/components/Header.vue
+++ tutorial/src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
 <div>
-  <b-nav>
-    <b-nav-item active>Quiz App</b-nav-item>
+  <b-nav tabs>
+    <b-nav-item disabled><b>Quiz App</b></b-nav-item>
     <b-nav-item>Counter: 4/10</b-nav-item>
   </b-nav>
 </div></template>
