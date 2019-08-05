fa3337eaa92332aca5d7c04a08ff9c25422186f7
Phone Nyo <phonenai2014@gmail.com>
2019-08-05 11:25:58 +0630

    Changing ques & answer

--- README.md
+++ README.md
@@ -10,3 +10,12 @@
 
 #localhost:8080
 
+#npm install bootstrap-vue bootstrap
+
+#import BootstrapVue from 'bootstrap-vue' (in main.js)
+#Vue.use(BootstrapVue)
+
+#import 'bootstrap/dist/css/bootstrap.css' (in main.js)
+#import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+#https://opentdb.com/api.php?amount=10&category=27&type=multiple 
--- tutorial/src/components/QuestionBox.vue
+++ tutorial/src/components/QuestionBox.vue
@@ -5,18 +5,48 @@
 	    	{{currentQuestion.question}}
 	    </template>
 	    <hr class="my-4">
-	    <p>
-	      List of answers
-	    </p>
+	    <b-list-group>
+		  <b-list-group-item v-for="(answer, index) in answers" :key="index">
+		  	{{ answer }}
+		  </b-list-group-item>
+		</b-list-group>
 	    <b-button variant="primary" href="#">Submit</b-button>
-	    <b-button variant="success" href="#">Next</b-button>  
+	    <b-button variant="success" @click="next" href="#">Next</b-button>  
 	</b-jumbotron>
 </div>
 </template>
 <script>
 	export default {
 		props: {
-			currentQuestion: Object
+			currentQuestion: Object,
+			next: Function
+		},
+		computed: {
+			answers() {
+				let answers = [...this.currentQuestion.incorrect_answers]  //spread opreator
+				answers.push(this.currentQuestion.correct_answer)
+				return answers
+			}
 		}
+		//it means specifying only the properties you want and use the spread operator for the rest
+		//But in this case, it takes only incorrect_answers from object of currentQuestion 
+		//And it is also called rest operator that can take an indefinite number of arguments,
+			// function sum(...numbers) {
+			// 	return numbers.reduce((accumulator, current) => {
+			// 		return accumulator += current;
+			// 	});
+			// };
+			 
+			// sum(1,2) // 3
+			// sum(1,2,3,4,5) // 15
+
 	}
 </script>
+<style scoped>
+	.list-group {
+		margin-bottom: 15px;
+	}
+	.btn {
+		margin: 0 15px;
+	}
+</style>
